bd6fc35f412ea0b7efb88408d515b7eeab0ad5af
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-11-04 09:28:31 -0500

    fullcalendar reduced test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,46 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import listPlugin from "@fullcalendar/list"
+
+const AFSPCalendar = () => {
+  const [allEvents, setAllEvents] = useState([])
+
+  useEffect(() => {
+    if (allEvents.length === 0 && allEvents[0] !== "no events") {
+      fetch(`//aws-fetch.s3.amazonaws.com/events/merged-programs.json`)
+        .then(response => {
+          if (response.status >= 400) {
+            throw new Error("Bad response from server")
+          }
+          return response.json()
+        })
+        .then(response => {
+          setAllEvents(response.programEvents)
+        })
+    }
+  }, [allEvents])
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        events={allEvents}
+        buttonText={{
+          dayGridMonth: "Month",
+          listWeek: "List",
+          today: "Today",
+        }}
+        headerToolbar={{
+          start: "title", // will normally be on the left. if RTL, will be on the right
+          center: "dayGridMonth,listWeek",
+          end: "today prev,next", // will normally be on the right. if RTL, will be on the left
+        }}
+      />
+    </div>
+  )
 }
+
+export default AFSPCalendar
